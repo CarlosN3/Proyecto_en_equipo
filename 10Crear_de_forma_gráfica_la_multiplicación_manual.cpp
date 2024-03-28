7be7be98df3908249dcc4ffab6c80d4c0ed42f3c
lrivasc4 <160422579+lrivasc4@users.noreply.github.com>
2024-03-27 21:21:20 -0600

    new files

---
+++ 10Crear_de_forma_gráfica_la_multiplicación_manual.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num1, num2, resultado = 0;
+
+    cout << "Ingrese el primer número: ";
+    cin >> num1;
+
+    cout << "Ingrese el segundo número: ";
+    cin >> num2;
+
+    for (int i = 1; i <= num2; i++) {
+        resultado += num1;
+        cout << "Paso " << i << ": " << num1 << " x " << i << " = " << resultado << endl;
+    }
+
+    cout << "El producto de " << num1 << " y " << num2 << " es " << resultado << ".";
+    
+    return 0;
+}
