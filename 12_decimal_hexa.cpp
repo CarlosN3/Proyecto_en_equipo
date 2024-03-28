a43bc859cb20ab2cb68841d11d143e830a8cd82e
wilmer marroquin <ios8.wil@gmail.com>
2024-03-28 10:55:17 -0600

    Actualizacion de Conversión de números decimales a hexadecimales

--- 12_decimal_hexa.cpp
+++ 12_decimal_hexa.cpp
@@ -4,16 +4,20 @@
 using namespace std;
 
 long long decimal;
-
+char respuesta= 's';
 int main(){
 	
-	    
-    cout << "Ingresa un número decimal: ";
+	while(respuesta== 'S'|| respuesta== 's'){
+	cout<<"El progrema le permite convertir un numoro escrito de manera Decimal a Hexadecimal"<<endl;	
+    cout << "Ingresa un numero decimal: ";
     cin >> decimal;
 
     cout << "El equivalente hexadecimal es: ";
     cout << hex << uppercase << setw(8) << setfill('0') << decimal << endl;
-	
+    cout<< "\n";
+    cout<<"Desea convertir otro numero (s/n)";
+    cin>> respuesta;
+	}
 
     return 0;
 }
