35a257321d8a05c9f4742ea563dedaeea09da171
lrivasc4 <160422579+lrivasc4@users.noreply.github.com>
2024-03-29 21:23:33 -0600

    nuevas correcciones

--- Menu.cpp
+++ Menu.cpp
@@ -7,8 +7,8 @@
 #include "6_conversionNumLetras.cpp"
 #include "7numeros_con_decimalaletras.cpp"
 #include "8Una_tabla_de_multiplicar.cpp"
-#include "9Todas_las_tablas_de_multiplicar_del_1_al_10..cpp"
-#include "10Crear_de_forma_gráfica_la_multiplicación_manual.cpp"
+#include "9Todas_las_tablas_de_multiplicar_del_1_al_10.cpp"
+#include "10Crear_de_forma_grafica_la_multiplicacion_manual.cpp"
 using namespace std;
 
 void opmatematicas();
@@ -20,7 +20,7 @@ void enteros_a_letras();
 void enteros_con_decimal_a_letras();
 void tabla_de_multiplicar();
 void tablas_de_multiplicar_del_1_al_10();
-void multiplicación_manual();
+void multiplicacion_manual();
 
 main () {
 
@@ -63,7 +63,7 @@ int option;
                 enteros_a_letras();
                 break;
             case 7:
-                convertirNumeroEnPalabras();
+                enteros_con_decimal_a_letras();
                 break;
             case 8:
                 tabla_de_multiplicar();
